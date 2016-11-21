f5c244b6248d2ce966085dbcf6e8992d0bce9b9d
Sachin Karia <sachin.karia01@gmail.com>
2016-11-21 12:30:49 +0000

    workaround to  promise error and added styling

--- rentoo-auth-angular/MainCtrl.js
+++ rentoo-auth-angular/MainCtrl.js
@@ -1,13 +1,29 @@
 app.controller('MainCtrl', ['$scope','$auth',function($scope, $auth){
-  $scope.test = 'Hello world!';
+  $scope.signedUp = false;
+  $scope.current_user = $auth.user;
 
   $scope.handleRegBtnClick = function() {
     $auth.submitRegistration($scope.registrationForm)
-      .then(function(resp) {
-        console.log('yay!')
-      })
-      .catch(function(resp) {
-        console.log('nooo')
-      });
+    .then(function() {
+      console.log('SUCCESS')
+    })
+    .catch(function(){
+      console.log('FAILED')
+    })
   };
+
+  $scope.handleLoginBtnClick = function() {
+    $auth.submitLogin($scope.registrationForm)
+    .then(function(resp) {
+      console.log('SUCCESS');
+    })
+    .catch(function(resp) {
+      console.log('FAILED.');
+    });
+  };
+
+  $scope.signUp = function(){
+    $scope.signedUp = true;
+  };
+
 }]);
--- rentoo-auth-angular/app.js
+++ rentoo-auth-angular/app.js
@@ -1,7 +1,48 @@
-var app = angular.module('Rentoo', ['ng-token-auth'])
+var app = angular.module('Rentoo', ['ng-token-auth','ui.router'])
 
-app.config(function($authProvider) {
+app.run(['$rootScope','$state', function($rootScope, $state) {
+  $rootScope.$on('auth:login-success', function() {
+    console.log('GREAT SUCCESS');
+    $state.go('home')
+  })
+  $rootScope.$on('auth:logout-success', function() {
+    console.log('GREAT SUCCESS');
+    $state.go('signup')
+  });
+}])
+
+app.config(['$stateProvider','$urlRouterProvider','$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
+    $stateProvider
+    .state('signup', {
+      url: '/',
+      views: {
+        content: {
+          templateUrl: 'templates/signup.html',
+          controller: 'MainCtrl'
+        }
+      }
+    })
+    .state('home', {
+      name: 'home',
+      url: '/home',
+      views: {
+        content: {
+          templateUrl: 'templates/home.html',
+          controller: 'MainCtrl'
+        },
+        resolve: {
+          auth: function($auth) {
+            return $auth.validateUser();
+          }
+        }
+      }
+    });
+
+
+  $urlRouterProvider.otherwise('/');
+
   $authProvider.configure({
     apiUrl: 'http://localhost:3000'
-  })
-});
+  });
+
+}]);
